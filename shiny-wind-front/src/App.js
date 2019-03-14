dd9657064e765ed5692f286da485ab4b8ab13894
Thomas Svartbäck <thomsva@gmail.com>
2019-03-14 15:08:38 +0200

    testing GoogleMapReact

--- shiny-wind-front/src/App.js
+++ shiny-wind-front/src/App.js
@@ -1,13 +1,40 @@
-import React from 'react'
 
+import React from 'react'
+import GoogleMapReact from 'google-map-react'
+
+
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>
 
 const App = () => {
-
+  const center = {
+    lat: 61,
+    lng: 25
+  }
+  const zoom = 7
 
   return (
     <div>
       <h1>Hello world</h1>
+
+
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'secret' }}
+          defaultCenter={center}
+          defaultZoom={zoom}
+
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
     </div>
+
+
   )
 }
 
